4c3ee6be12837620fa8d33497c6fb944ce7c32e5
Vaishnavikokate11 <162028120+Vaishnavikokate11@users.noreply.github.com>
2024-10-26 19:07:12 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_vA80s6RLP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '49mvs3jg79qbdtu6nic1s67nh1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
